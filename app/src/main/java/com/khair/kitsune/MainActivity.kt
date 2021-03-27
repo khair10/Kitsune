17d88595f35b8acada81a0ea2225d81c922c4d2c
Rustem Khairutdinov <hayr1998@gmail.com>
2021-03-27 18:07:12 +0300

    base retrofit enqueue, callback executor fixed
    Callback work on the background thread, so background thread can not
    change UI. Added Handler for threads communication.

--- app/src/main/java/com/khair/kitsune/MainActivity.kt
+++ app/src/main/java/com/khair/kitsune/MainActivity.kt
@@ -2,15 +2,22 @@ package com.khair.kitsune
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
 import android.widget.TextView
+import androidx.core.os.HandlerCompat
 import com.khair.kitsune.data.RetrofitHelper
 import com.khair.kitsune.data.ServiceProvider
 import com.khair.kitsune.models.remote.Response
 import retrofit2.Call
 import retrofit2.Callback
+import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var handler: Handler
 
     val retrofit = RetrofitHelper().retrofit
     val serviceProvider = ServiceProvider(retrofit)
@@ -24,22 +31,25 @@ class MainActivity : AppCompatActivity() {
 
         val tvText = findViewById<TextView>(R.id.tvText)
 
+        handler = Handler(Looper.getMainLooper()) {
+            Log.d("MainActivity", "message received")
+            tvText.text = it.obj.toString()
+            true
+        }
+
         call = animeService.getAnimes()
 
         call?.enqueue(object : Callback<Response> {
             override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
-                Log.d("MainActivity", Thread.currentThread().name + ": on response")
-                val body = response.body()
-                if (response.isSuccessful && body != null) {
-                    for (item in body.data) {
-                        Log.d("MainActivity", item.anime.toString())
-                    }
-                    tvText.text = body.data[0].anime.canonicalTitle
-                }
+                handler.sendMessage(
+                    handler.obtainMessage(
+                        1,
+                        response.body()?.data?.get(0)?.anime?.canonicalTitle
+                    )
+                )
             }
 
             override fun onFailure(call: Call<Response>, t: Throwable) {
-                Log.d("MainActivity", t.localizedMessage)
             }
         })
     }
@@ -47,5 +57,6 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         call?.cancel()
+        handler.removeCallbacksAndMessages(null)
     }
 }
