da8f0e6ad2bc9007c4327b80607d78df21e58c31
Rustem Khairutdinov <hayr1998@gmail.com>
2021-03-28 03:53:38 +0300

    base retrofit execute, async task
    Async task for backgroud call with retrofit execute method

--- app/src/main/java/com/khair/kitsune/MainActivity.kt
+++ app/src/main/java/com/khair/kitsune/MainActivity.kt
@@ -8,21 +8,15 @@ import com.khair.kitsune.data.RetrofitHelper
 import com.khair.kitsune.data.ServiceProvider
 import com.khair.kitsune.models.remote.Response
 import retrofit2.Call
-import java.lang.Exception
 import java.lang.ref.WeakReference
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var handler: Handler
-    private lateinit var executor: ExecutorService
 
     val retrofit = RetrofitHelper().retrofit
     val serviceProvider = ServiceProvider(retrofit)
     val animeService = serviceProvider.animeService
     var call: Call<Response>? = null
-    var runner: Runnable? = null
+    var async: AsyncTask<Void, Long, String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,48 +25,33 @@ class MainActivity : AppCompatActivity() {
 
         val tvText = findViewById<TextView>(R.id.tvText)
 
-        executor = Executors.newSingleThreadExecutor()
-        handler = Handler(Looper.getMainLooper()) {
-            Log.d("MainActivity", "message received")
-            tvText.text = it.obj.toString()
-            true
-        }
         call = animeService.getAnimes()
         call?.let {
-            runner = Runner(it, handler)
+            async = AsyncLoad(tvText, it)
         }
-        runner?.let {
-            executor.submit(runner)
-        }
+        async?.execute()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         call?.cancel()
-        executor.shutdownNow()
-        handler.removeCallbacksAndMessages(null)
+        async?.cancel(true)
     }
 
-    class Runner(call: Call<Response>, handler: Handler) : Runnable {
-        val call = WeakReference<Call<Response>>(call)
-        val handler = WeakReference<Handler>(handler)
+    class AsyncLoad(tvTextView: TextView, call: Call<Response>): AsyncTask<Void, Long, String>() {
+        val tvTextView = WeakReference(tvTextView)
+        val call = WeakReference(call)
 
-        override fun run() {
-            Log.d("MainActivity", Thread.currentThread().name + ": on create")
-            try {
-                val data = call.get()?.execute()
-                Thread.sleep(5000)
-                handler.get()?.let {
-                    it.sendMessage(
-                        it.obtainMessage(
-                            0,
-                            data?.body()?.data?.get(0)?.anime?.canonicalTitle
-                        )
-                    )
-                }
-            } catch (e: Exception) {
-                Log.d("MainActivity", e.localizedMessage)
-            }
+        override fun doInBackground(vararg params: Void?): String {
+            val response = call.get()?.execute()
+            Thread.sleep(5000)
+            Log.d("ASYNC_TASK", Thread.currentThread().name + " " + Thread.currentThread().id)
+            return response?.body()?.data?.get(0)?.anime?.canonicalTitle ?: "Empty"
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            tvTextView.get()?.text = result
         }
     }
 }
